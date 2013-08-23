bbf653b4b9f11056bf67662bf3af654650c8c6d3
Dave Keller <keller@infinitemule.com>
2013-08-22 20:41:25 -0400

    Added creating an image and saving to a ping file

--- src/main/scala/com/infinitemule/github/identicons/Identicon.scala
+++ src/main/scala/com/infinitemule/github/identicons/Identicon.scala
@@ -1,7 +1,15 @@
+/*
+ * GitHub Identicons
+ */
 package com.infinitemule.github.identicons
 
 import org.apache.commons.codec.digest.DigestUtils
 import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.IOException
+import java.io.File
+import javax.imageio.ImageIO
+
 
 object Identicon {
 
@@ -34,20 +42,7 @@ object Identicon {
     val color = createColor(hash.take(3))
         
     Identicon(glyph, color)
-  }
-  
-  
-  def print(username: String) = {
-    
-    val identicon = create(username)
-    
-    identicon.glyph.foreach { row =>
-      println(row.map { x => if(x == 1) "X" else " " }.mkString(""))      
-    }
-    
-    println("COLOR: " + identicon.color)
-    
-  }
+  }  
   
   
   /**
@@ -59,21 +54,90 @@ object Identicon {
    * - Take a color in RGB 0-255
    * - Covert it to HSB so that we can get the hue
    * - Create a new color with a fixed saturation and value
-   * - Return it as a hex string
    * 
    * TODO Make the saturation and value either customizable
    *      or within a range based on the hash.
    */
-  def createColor(rgb: List[Int]): String = {       
+  def createColor(rgb: List[Int]): Color = {       
     
     val hsb = Color.RGBtoHSB(rgb(0), rgb(1), rgb(2), null)
     
-    val color = new Color(Color.HSBtoRGB(hsb(0), 0.45f, 0.80f))
-        
-    "%02x%02x%02x".format(color.getRed(), color.getGreen(), color.getBlue())
-    
+    new Color(Color.HSBtoRGB(hsb(0), 0.45f, 0.80f))
+                
   }
+      
     
 }
 
-case class Identicon(val glyph: List[List[Int]], val color: String)
+case class Identicon(val glyph: List[List[Int]], val color: Color) {
+  
+  
+  def hexColor(): String = {
+     "%02x%02x%02x".format(
+            color.getRed(), 
+            color.getGreen(), 
+            color.getBlue())    
+  }
+  
+  
+  /**
+   * Creates a BufferedImage version of an identicon
+   */ 
+  def createImage(): BufferedImage = {
+    
+    val img = new BufferedImage(420, 420, BufferedImage.TYPE_INT_RGB)
+    val gfx = img.createGraphics()
+
+    gfx.setColor(new Color(240, 240, 240))
+    gfx.fillRect(0, 0, img.getWidth(), img.getHeight())    
+    gfx.setColor(color)
+    
+    /*
+     * - Index both rows
+     * - Filter only rows with pixel turned on
+     * - Map coordinates to the on pixels
+     * - Flatten the list to get a list of coords for
+     *   each on pixel (i.e ((0,1), (0,2), (0,3), (1,0), ...
+     */
+    val onPixels =  glyph.map { _.zipWithIndex } .zipWithIndex
+     .map { case(row, i) => 
+       row.filter { case(bit, j) => bit == 1 } 
+       .map { case(bit, j) => (j, i) } 
+     } 
+     .flatten
+
+    onPixels.map { case(x, y) => gfx.fillRect(x * 84, y * 84, 84, 84) } 
+    
+    img
+  }
+
+  
+  /**
+   * Saves an identicon to a PNG file.
+   */
+  def saveImage(filename: String) = {
+    
+    try {
+      ImageIO.write(createImage(), "png", new File(filename))
+    } 
+    catch { 
+      case e: IOException =>
+        throw new RuntimeException("Error saving image.", e)
+    }
+
+  }
+    
+    
+  /**
+   * Prints an identicon to the console
+   */
+  def print() = {
+        
+    glyph.foreach { row =>
+      println(row.map { x => if(x == 1) "X" else " " }.mkString(""))      
+    }
+    
+    println("COLOR: " + hexColor)
+    
+  }
+}
